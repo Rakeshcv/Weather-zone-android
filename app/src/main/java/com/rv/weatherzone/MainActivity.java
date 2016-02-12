db02a7fd08175e2864e43798e2a6389a7cc94964
Rakesh C V <rakesh.cv@hotmail.com>
2016-02-12 21:18:55 +1100

    making Async call

--- app/src/main/java/com/rv/weatherzone/MainActivity.java
+++ app/src/main/java/com/rv/weatherzone/MainActivity.java
@@ -1,13 +1,57 @@
 package com.rv.weatherzone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "cc217ebdc7e2d5685cce923661e73910";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(forecastUrl).build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    Log.e(TAG, response.body().string());
+                    if (response.isSuccessful()) {
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+
+            }
+        });
+
     }
 }
